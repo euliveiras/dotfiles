40a186110d8322737294dfd0d713038783a274c3
euliveiras <matheus.skm@hotmail.com>
2025-12-28 18:58:28 -0300

    refactor: lsp

--- nvim/lua/euliveiras/lazy/lspconfig.lua
+++ nvim/lua/euliveiras/lazy/lspconfig.lua
@@ -1,129 +1,42 @@
-return {{
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "mason.nvim",
-     "mason-org/mason-lspconfig.nvim"  }
-     ,
-    servers = {
-      --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
-      --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
-      ---
-      dartls = {},
-      ruff = {
-        cmd_env = { RUFF_TRACE = "messages" },
-        init_options = {
-          settings = {
-            logLevel = "error",
-          },
-        },
-      },
-      ruff_lsp = {
-      },
-      omnisharp = {
-        handlers = {
-          ["textDocument/definition"] = function(...)
-            return require("omnisharp_extended").handler(...)
-          end,
-        },
-        keys = {
-          {
-            "gd",
-            desc = "Goto Definition",
-          },
-        },
-        enable_roslyn_analyzers = true,
-        organize_imports_on_format = true,
-        enable_import_completion = true,
-      },
-
-      tsserver = {
-        enabled = true,
-      },
-      ts_ls = {
-        enabled = true,
-      },
-      vtsls = {
-        -- explicitly add default filetypes, so that we can extend
-        -- them in related extras
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx",
-          "typescript",
-          "typescriptreact",
-          "typescript.tsx",
-        },
-        settings = {
-          complete_function_calls = true,
-          vtsls = {
-            enableMoveToFileCodeAction = true,
-            autoUseWorkspaceTsdk = true,
-            experimental = {
-              maxInlayHintLength = 30,
-              completion = {
-                enableServerSideFuzzyMatch = true,
-              },
-            },
-          },
-          typescript = {
-            updateImportsOnFileMove = { enabled = "always" },
-            suggest = {
-              completeFunctionCalls = true,
-            },
-            inlayHints = {
-              enumMemberValues = { enabled = true },
-              functionLikeReturnTypes = { enabled = true },
-              parameterNames = { enabled = "literals" },
-              parameterTypes = { enabled = true },
-              propertyDeclarationTypes = { enabled = true },
-              variableTypes = { enabled = false },
-            },
-          },
-        },
-        },
-      },
-      clangd = {
-        keys = {
-          { "<leader>ch", "<cmd>LspClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-        },
-        root_markers = {
-          "compile_commands.json",
-          "compile_flags.txt",
-          "configure.ac", -- AutoTools
-          "Makefile",
-          "configure.ac",
-          "configure.in",
-          "config.h.in",
-          "meson.build",
-          "meson_options.txt",
-          "build.ninja",
-          ".git",
-        },
-        capabilities = {
-          offsetEncoding = { "utf-16" },
-        },
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders",
-          "--fallback-style=llvm",
-        },
-        init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-        },
-      },
-    },
-
-    {
-    "mason-org/mason-lspconfig.nvim",
-    },
+return {
+    "neovim/nvim-lspconfig",
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
+        "stevearc/conform.nvim",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
     },
+
+    config = function()
+        local cmp = require('cmp')
+        require("mason").setup()
+        require("mason-lspconfig").setup()
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = "copilot", group_index = 2 },
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
+    end
 }
--- nvim/lua/plugins/mason.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "mason-org/mason.nvim",
-  opts = { ensure_installed = { "csharpier", "netcoredbg", "fantomas" } },
-}
