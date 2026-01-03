bb8c956fe8eed9b2f98d6b0c94f66505bcb037b5
euliveiras <matheus.skm@hotmail.com>
2026-01-03 14:45:36 -0300

    debugger

---
+++ nvim/lua/euliveiras/lazy/debugger.lua
@@ -0,0 +1,181 @@
+return {
+  {
+    "theHamsta/nvim-dap-virtual-text",
+  },
+  { "nvim-neotest/nvim-nio" },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "jay-babu/mason-nvim-dap.nvim",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    config = function()
+      require("mason").setup({})
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "python", "chrome" },
+        automatic_installation = true,
+      })
+      require("nvim-dap-virtual-text").setup({})
+      local dap = require("dap")
+      local debugger_path = vim.env.HOME .. "/www/js-debug/src/dapDebugServer.js"
+      local ui = require("dapui")
+
+      ui.setup({})
+
+      vim.fn.sign_define("DapBreakpoint", { text = "B" })
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          -- 💀 Make sure to update this path to point to your installation
+          args = { debugger_path, "${port}" },
+        },
+      }
+
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        dap.configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
+    keys = {
+      {
+        "<leader>d",
+        group = "Debugger",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>dt",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle Breakpoint",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>dc",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>di",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>do",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>du",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>dr",
+        function()
+          require("dap").repl.open()
+        end,
+        desc = "Open REPL",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>dl",
+        function()
+          require("dap").run_last()
+        end,
+        desc = "Run Last",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>dq",
+        function()
+          require("dap").terminate()
+          require("dapui").close()
+          require("nvim-dap-virtual-text").toggle()
+        end,
+        desc = "Terminate",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>db",
+        function()
+          require("dap").list_breakpoints()
+        end,
+        desc = "List Breakpoints",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>de",
+        function()
+          require("dap").set_exception_breakpoints({ "all" })
+        end,
+        desc = "Set Exception Breakpoints",
+        nowait = true,
+        remap = false,
+      },
+    },
+  },
+}
--- nvim/lua/euliveiras/lazy/lspconfig.lua
+++ nvim/lua/euliveiras/lazy/lspconfig.lua
@@ -1,42 +1,42 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "stevearc/conform.nvim",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-    },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "stevearc/conform.nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+  },
 
-    config = function()
-        local cmp = require('cmp')
-        require("mason").setup()
-        require("mason-lspconfig").setup()
+  config = function()
+    local cmp = require("cmp")
+    require("mason").setup()
+    require("mason-lspconfig").setup()
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = "copilot", group_index = 2 },
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            })
-        })
-    end
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<S-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = "copilot", group_index = 2 },
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+      }, {
+        { name = "buffer" },
+      }),
+    })
+  end,
 }
